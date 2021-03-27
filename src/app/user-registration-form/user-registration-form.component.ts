ca20e5668af07beaf60e9d9f52ab49bab71cfdd5
KSFlynn007 <kelseyflynnn@gmail.com>
2021-03-26 18:27:09 -0600

    Update user-registration-form.component.ts

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -30,8 +30,8 @@ registerUser(): void {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(response);
-     this.snackBar.open('user registered successfully!', 'OK', {
-        duration: 2000
+     this.snackBar.open('User was registered successfully, you can now login!', 'OK', {
+        duration: 5000
      });
     }, (response) => {
       console.log(response);
